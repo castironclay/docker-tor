5752012fb9525f1941cf3ebbada4beddec48b770
castironclay <17088350+castironclay@users.noreply.github.com>
2021-07-10 23:15:20 -0400

    Update start script

--- skyhook.sh
+++ skyhook.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 docker run -it --cap-add=NET_ADMIN \
         --dns 127.0.0.1 \
+        --env ALL_PROXY="socks5h://127.0.0.1:9050" \
+        --env HTTPS_PROXY="socks5h://127.0.0.1:9050" \
+        --env HTTP_PROXY="socks5h://127.0.0.1:9050" \
         --add-host socks5h:127.0.0.11 \
         --add-host dns4torpnlfs2ifuz2s2yf3fc7rdmsbhm6rw75euj35pac6ap25zgqad.onion:127.0.0.1 \
-	      docker-tor /bin/bash
+	      castironclay/docker-tor:latest /bin/bash
